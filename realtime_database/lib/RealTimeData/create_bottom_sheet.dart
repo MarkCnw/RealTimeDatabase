5004443c708126a8c7c54722522d25ac938f4ad0
MarkCnw <chinnawong554@gmail.com>
2025-02-10 00:49:19 +0700

    Implement create and update bottom sheets for item management and add Firebase URL to configuration

--- realtime_database/lib/RealTimeData/create_bottom_sheet.dart
+++ realtime_database/lib/RealTimeData/create_bottom_sheet.dart
@@ -1,56 +1,79 @@
+
 import 'package:flutter/material.dart';
 import 'package:realtime_database/RealTimeData/realtime_crud.dart';
 
 final TextEditingController nameController = TextEditingController();
 final TextEditingController snController = TextEditingController();
 final TextEditingController addressController = TextEditingController();
-void createButtonsheet(BuildContext context) {
+
+void createBottomSheet(BuildContext context) {
   showModalBottomSheet(
+      isScrollControlled: true,
       backgroundColor: Colors.blue[100],
       context: context,
       builder: (BuildContext context) {
         return Padding(
-          padding: EdgeInsets.only(top: 20, right: 20, left: 20, bottom: MediaQuery.of(context).viewInsets.bottom + 20),
+          padding: EdgeInsets.only(
+            top: 20,
+            right: 20,
+            left: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 20,
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
-              Center(
+              const Center(
                 child: Text(
                   "Create your items",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               TextField(
                 controller: nameController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: "Name",
-                  hintText: "Eg.elon",
+                  hintText: "eg.Elon",
                 ),
               ),
               TextField(
+                keyboardType: TextInputType.number,
                 controller: snController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: "S.N",
                   hintText: "eg.1",
                 ),
               ),
               TextField(
-                controller: nameController,
-                decoration: InputDecoration(
+                controller: addressController,
+                decoration: const InputDecoration(
                   labelText: "Address",
                   hintText: "eg.UK",
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               ElevatedButton(
                   onPressed: () {
                     final id = DateTime.now().microsecond.toString();
-                    databaseReference.child(DateTime.now().millisecondsSinceEpoch.toString()).set({'name': nameController.text, 'sn': snController.text, 'address': addressController.text});
+                    databaseReference.child(id).set({
+                      'name': nameController.text.toString(),
+                      'sn': snController.text.toString(),
+                      'address': addressController.text.toString(),
+                      'id': id //It's give the unique id every time.
+                    });
+                    // For clear the controller
+                    nameController.clear();
+                    snController.clear();
+                    addressController.clear();
+                    //For Dismiss the keyboard afte adding items
+                    Navigator.pop(context);
                   },
-                  child: Text("add"))
+                  child: const Text("add"))
             ],
           ),
         );
--- realtime_database/lib/RealTimeData/realtime_crud.dart
+++ realtime_database/lib/RealTimeData/realtime_crud.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:realtime_database/RealTimeData/create_buttom.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:realtime_database/RealTimeData/create_bottom_sheet.dart';
+import 'package:realtime_database/RealTimeData/update_bottom.dart';
 
 class RealTimeCruddatabase extends StatefulWidget {
   const RealTimeCruddatabase({super.key});
@@ -8,21 +11,80 @@ class RealTimeCruddatabase extends StatefulWidget {
   @override
   State<RealTimeCruddatabase> createState() => _RealTimeCruddatabaseState();
 }
-final databaseReference = FirebaseDatabase.instance.ref();
+
+// final nameController = TextEditingController();
+final databaseReference = FirebaseDatabase.instance.ref("StoreData");
+
 class _RealTimeCruddatabaseState extends State<RealTimeCruddatabase> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: Colors.blue[100],
       appBar: AppBar(
         backgroundColor: Colors.blue,
         centerTitle: true,
-        title: Text('Realtime Database'),
+        title: const Text("Realtime Database"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: FirebaseAnimatedList(
+                  query: databaseReference,
+                  itemBuilder: (context, snapshot, index, animation) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                      margin: const EdgeInsets.all(10),
+                      child: ListTile(
+                        title: Text(snapshot.child("name").value.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                        subtitle: Text(snapshot.child("address").value.toString()),
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.blue[100],
+                          child: Text(snapshot.child("sn").value.toString())),
+                        trailing: PopupMenuButton(
+                          icon: const Icon(Icons.more_vert),
+                          itemBuilder: (context) => [
+                            PopupMenuItem(
+                              value: 1,
+                              child: ListTile(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  updateBottomSheet(
+                                    context,
+                                    snapshot.child("name").value.toString(),
+                                    snapshot.child("id").value.toString(),
+                                    snapshot.child("sn").value.toString(),
+                                    snapshot.child("address").value.toString(),
+                                  );
+                                },
+                                leading: const Icon(Icons.edit),
+                                title: const Text("Edit"),
+                              ),
+                            ),
+                            PopupMenuItem(
+                                value: 2,
+                                child: ListTile(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    databaseReference.child(snapshot.child('id').value.toString()).remove();
+                                  },
+                                  leading: const Icon(Icons.delete),
+                                  title: const Text(
+                                    "Delete",
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                    );
+                  }))
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => createButtonsheet(context),
-        child: Icon(Icons.add),
-        backgroundColor: Colors.blue,
+        onPressed: () => createBottomSheet(context),
+        child: const Icon(Icons.add),
       ),
     );
   }
